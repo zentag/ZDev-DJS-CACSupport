603b65530e0b2288545d40b0088381c661794e18
zentag <zentagfw@gmail.com>
2021-03-02 21:44:17 -0500

    Version 1.2
    Added -suggest

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { Server } = require("ws");
 const client = new Discord.Client();
-var modmailUser = 0;
+var modmailUser = '521115847801044993';
 var mod = 'Infinity_Oofs#3438'
 
 client.on('ready', () => {
@@ -15,7 +15,7 @@ client.on('message', msg => {
   }
   if(msg.channel.type === 'dm') {
     if(msg.author.bot === false){
-      client.channels.cache.get('816423421323640853').send(msg.author.tag + '(' + msg.author.id + ')' + '>>' + msg.content.replace('-modmail ', ''));
+      client.channels.cache.get('816423421323640853').send('@here ' + msg.author.tag + '(' + msg.author.id + ')' + '>>' + msg.content.replace('-modmail ', ''));
     }
   }
   if (msg.content.startsWith('-user')) {
@@ -29,6 +29,18 @@ client.on('message', msg => {
     else{
       msg.reply('sorry, you do not have permission to do this command.')
     }
+  }
+  if(msg.content.startsWith('-suggest')){
+    const suggestion = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Suggestion from ' + msg.author.tag)
+      .setFooter('Suggestion by ' + msg.author.tag + ' | Version 1.2')
+      .setDescription(msg.content.replace('-suggest ', ''));
+
+      client.channels.cache.get('807046119950778419').send(suggestion).then(function (message) {
+        message.react("✅")
+        message.react("❌")
+      })
   }
 });
 
