936bae5c1a4f075f220a5047d9ed7a5558f9c4fb
zentag <zentagfw@gmail.com>
2021-03-03 20:09:30 -0500

    Version 1.4
    Added -ticket and -end (user id)

--- bot.js
+++ bot.js
@@ -3,20 +3,49 @@ const { Server } = require("ws");
 const client = new Discord.Client();
 var modmailUser = '521115847801044993';
 var mod = 'Infinity_Oofs#3438'
-const version = 1.3
+const version = 1.4
+const hasTicket = new Set()
 
 client.on('ready', () => {
-     console.log("Ready");
-     client.user.setActivity("CraftACat || Just DM", { type: "PLAYING"})
-
+  console.log("Ready");
+  client.user.setActivity("CraftACat || Just DM", { type: "PLAYING"});
 });
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if(msg.content.startsWith('-end')){
+    if(msg.member.roles.cache.find(r => r.name === "Support Team")){
+      hasTicket.delete(msg.content.replace('-end ', ''))
+      const ticketEnd = new Discord.MessageEmbed()
+        .setTitle('Ticket closed by staff ' + msg.author.tag)
+        .setDescription('Your ticket has been marked as resolved. To add another ticket do -ticket.')
+        .setColor('#0099ff')
+        .setFooter('Ticket | Version ' + version);
+      client.users.cache.get(msg.content.replace('-end ', '')).send(ticketEnd)
+      msg.reply('You have ended user: ' + msg.content.replace('-end ', '') + "'s ticket.")
+    }
+    else{
+      msg.reply('Sorry, you do not have permssion to do this command.')
+    }
+  }
+  if (msg.content.startsWith('-ticket')) {
+    if(hasTicket.has(msg.author.id) === false){
+      const ticketStart = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Your ticket has been opened.')
+        .setFooter('Ticket | Version ' + version)
+        .setDescription('Please state your problem now. Staff have been pinged and will respond shortly. From now on you can DM the bot directly.');
+      client.users.cache.get(msg.author.id).send(ticketStart)
+      client.channels.cache.get('816423421323640853').send('@here ' + msg.author.tag + ' has started a ticket (' + msg.author.id + ')');
+      hasTicket.add(msg.author.id)
+      msg.delete()
+    }
+    else{
+      msg.delete()
+      msg.reply('sorry, you already have a ticket open')
+    }
   }
   if(msg.channel.type === 'dm') {
     if(msg.author.bot === false){
-      client.channels.cache.get('816423421323640853').send('@here ' + msg.author.tag + '(' + msg.author.id + ')' + '>>' + msg.content.replace('-modmail ', ''));
+      client.channels.cache.get('816423421323640853').send(msg.author.tag + ' (' + msg.author.id + ') ' + '>> ' + msg.content);
     }
   }
   if (msg.content.startsWith('-user')) {
@@ -45,10 +74,10 @@ client.on('message', msg => {
   }
   if(msg.content === '-help'){
     const help = new Discord.MessageEmbed()
-      .setColor('0099ff')
+      .setColor('#0099ff')
       .setTitle('Commands')
       .setFooter('Help Menu | Version ' + version)
-      .setDescription('**-help** - Brings up this menu \n**-suggest** (suggestion) - Puts a suggestion in the suggestion channel \n**Getting Support** - Just DM this bot to send a message to staff ');
+      .setDescription('**-help** - Brings up this menu \n**-suggest** (suggestion) - Puts a suggestion in the suggestion channel \n**-ticket** - Opens a new ticket which pings staff\n**Getting Support** - Just DM this bot to send a message to staff ');
     msg.channel.send(help)
   }
 });
