94c35abd165e0792c6dd6598d82816afc17fbf6a
zentag <zentagfw@gmail.com>
2021-04-04 01:34:07 -0400

    eqd
    qw

--- features/message.js
+++ features/message.js
@@ -2,7 +2,12 @@ module.exports = (client) => {
     client.on("message", (message) => {
         if(message.channel.type === "dm"){
             if(!message.author.bot){
-                
+                if(hasTicket.has(message.author.id)){
+                    client.channels.cache.get("816423421323640853").send(message.author.tag + " (" + message.author.id + ") >>" + message.content)
+                }
+                else{
+                    message.reply("Sorry, you don't have a ticket open. Open one with -ticket")
+                }
             }
             
         }
